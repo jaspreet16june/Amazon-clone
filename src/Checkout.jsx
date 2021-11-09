ba8fa37388daaec6d724421c2554e8ae6f5fe6ea
jaspreet kaur <jaspreetkaur16june@gmail.com>
2021-11-09 19:48:20 +0530

    CheckOut functionality completed

--- src/Checkout.jsx
+++ src/Checkout.jsx
@@ -2,34 +2,46 @@ import React from 'react'
 import { useStateValue } from './StateProvider';
 import "./css/checkout.css"
 import CheckoutProduct from "./CheckoutProduct.jsx"
+import Subtotal from "./Subtotal.jsx"
 
 const Checkout = () => {
     const [{basket}] = useStateValue();
     return (
-      <div classname="checkout">
-        <img
-          className="checkout_ad"
-          src="https://images-eu.ssl-images-amazon.com/images/G/31/img2020/OHL/Mar_HSS/Clearance_1500x300_gifud.gif"
-          alt="checkOut image"
-        />
+      <div className="checkout">
+      <div className ="checkout_left">
+          <img
+            className="checkout_ad"
+            src="https://images-eu.ssl-images-amazon.com/images/G/31/img2020/OHL/Mar_HSS/Clearance_1500x300_gifud.gif"
+            alt="checkOut image"
+          />
         {basket?.length === 0 ? (
-            <div>
-                <h2>Your Shopping Basket is empty</h2>
-                <p>You have no items in your basket.To buy one or more items click to "Add to basket" next to the item</p>
-            </div>
+          <div>
+            <h2>Your Shopping Basket is empty</h2>
+            <p>
+              You have no items in your basket.To buy one or more items click to
+              "Add to basket" next to the item
+            </p>
+          </div>
         ) : (
-            <div>
-                <h2 className = "checkout_title"> Your shopping basket</h2>
-                {basket.map(item=>(
-                    <CheckoutProduct 
-                    id = {item.id}
-                    name = {item.name}
-                    price = {item.price}
-                    rating = {item.rating}
-                    image ={item.image}
-                    />
-                ))}
-            </div>
+          <div>
+            <h2 className="checkout_title"> Your shopping basket</h2>
+            {basket?.map((item) => (
+              <CheckoutProduct
+                id={item.id}
+                name={item.name}
+                price={item.price}
+                rating={item.rating}
+                image={item.image}
+              />
+            ))}
+          </div>
+        )}
+        </div>
+        {basket.length > 0 && (
+          <div className="checkout_right">
+            <h1>hiiiii subtotal this side </h1>
+            {/* <Subtotal /> */}
+          </div>
         )}
       </div>
     );
